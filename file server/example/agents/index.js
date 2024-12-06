1a65533665b5e1da6137cb36bffa15bb73bf95a2
Anna Puddles <113144806+annapuddles@users.noreply.github.com>
2024-12-06 14:59:25 -0500

    Add position column, use innerText

--- file server/example/agents/index.js
+++ file server/example/agents/index.js
@@ -8,16 +8,20 @@ window.addEventListener('load', function() {
             let row = document.createElement('tr');
 
             let col1 = document.createElement('td');
-            col1.innerHTML = agent.key;
+            col1.innerText = agent.key;
             row.append(col1);
 
             let col2 = document.createElement('td');
-            col2.innerHTML = agent.displayName;
+            col2.innerText = agent.displayName;
             row.append(col2);
             
             let col3 = document.createElement('td');
-            col3.innerHTML = agent.username;
+            col3.innerText = agent.username;
             row.append(col3);
+            
+            let col4 = document.createElement('td');
+            col4.innerText = agent.position;
+            row.append(col4);
 
             table.append(row);
         });
